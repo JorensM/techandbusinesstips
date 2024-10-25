e4b1006fec1bb305e7ae3ca1455c88b98ddeba5f
JorensM <jorensmerenjanu@gmail.com>
2024-10-25 19:02:13 +0300

    fix: remove lastModified XML tag

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -13,8 +13,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
             url: websiteURL
         },
         ...posts.map(post => ({
-            url: postsURL + post.slug,
-            lastModified: post.date
+            url: postsURL + post.slug
         }))
     ]
 }
